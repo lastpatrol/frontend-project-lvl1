2a2e4ac83dadb3c4131e2b480b912cf175a491b0
lastpatrol <lastpatrol09@gmail.com>
2020-12-13 21:37:14 +0300

    minor optimization in isPrime func in brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,15 +6,8 @@ const isPrime = (num) => {
     return false;
   }
 
-  if (num === 2) {
-    return true;
-  }
-
-  if (num % 2 === 0) {
-    return false;
-  }
-
-  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+  const sqrt = Math.sqrt(num);
+  for (let i = 2; i <= sqrt; i += 1) {
     if (num % i === 0) {
       return false;
     }
